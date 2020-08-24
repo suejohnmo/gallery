9a0e9bf3a510814e729b5436d22916fb78cd092c
suejohnmo <susan.mwaura@gmail.com>
2020-08-25 00:26:45 +0300

    Milestone1

--- _config.js
+++ _config.js
@@ -1,9 +1,12 @@
-var config = {}
+var config = {};
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+  production:
+    "mongodb+srv://suejohnmo:Seiko@1979@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority",
+  development:
+    "mongodb+srv://suejohnmo:Seiko@1979@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority",
+  test:
+    "mongodb+srv://suejohnmo:Seiko@1979@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority",
+};
 module.exports = config;
